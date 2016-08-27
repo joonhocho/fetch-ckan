3b16e140b94a957321a14aaeb8d4f13823d07da6
Joon Ho Cho <joonho1101@gmail.com>
2016-08-27 06:50:20 -0700

    without undefined

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import fetch from 'node-fetch';
 import querystring from 'querystring';
 
 
+const withoutUndefined = (obj) => {
+  const dest = {};
+  for (const key of Object.keys(obj)) {
+    const val = obj[key];
+    if (val !== undefined) {
+      dest[key] = val;
+    }
+  }
+  return dest;
+};
+
 // http://docs.ckan.org/en/latest/maintaining/datastore.html
 // https://inventory.data.gov/dataset/032e19b4-5a90-41dc-83ff-6e4cd234f565/resource/38625c3d-5388-4c16-a30f-d105432553a4?filters=OPEID%3A100200
 const fetchApi = (endpoint, {
@@ -15,21 +26,20 @@ const fetchApi = (endpoint, {
   filters,
   ...rest,
 }) => {
-  const qs = querystring.stringify({
-    filters: filters && JSON.stringify(filters),
-    ...rest,
-  });
+  const query = withoutUndefined(rest);
+  if (filters && Object.keys(filters).length) {
+    query.filters = JSON.stringify(filters);
+  }
+
+  const qs = querystring.stringify(query);
+
   return fetch(`${endpoint}?${qs}`).then((res) => res.json());
 };
 
 
-const fetchRecords = (...args) => fetchApi(...args).then((response) => {
-  if (response.success) {
-    return response.result.records;
-  }
-
-  return [];
-});
+const fetchRecords = (...args) => fetchApi(...args).then((response) =>
+  response.success ? response.result.records : []
+);
 
 
 export {
